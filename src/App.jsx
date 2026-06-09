5966b54129096ebd1adf98bf255e041fc816f0fe
Daniel Brett <drummer.man.dan@icloud.com>
2026-06-09 10:10:19 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { useState } from 'react';
 import './App.css'
 
 
 
 function App() {
+  const [data, setData] = useState(null);
+
   async function loadData() {
     const response = await fetch('https://api.worldbank.org/v2/country/USA/indicator/NY.GDP.MKTP.CD?format=json');
     const data = await response.json();
-    console.log(data);
+    setData(json[1]);
   }
-  
+
   return (
     <div>
       <h1>Global Indicators</h1>
       <button onClick={loadData}>Load data</button>
+      {data && <p>Loaded {data.length} records</p>}
     </div>
   )
 }
