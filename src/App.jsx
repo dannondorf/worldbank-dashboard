9750ca7dfc1a751ed438d2ec2120f1e9beb17f50
Daniel Brett <drummer.man.dan@icloud.com>
2026-06-09 07:48:53 -0400

    file structure cleanup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+
+
+function App() {
+  async function loadData() {
+    const response = await fetch('https://api.worldbank.org/v2/country/USA/indicator/NY.GDP.MKTP.CD?format=json');
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  return (
+    <div>
+      <h1>Global Indicators</h1>
+      <button onClick={loadData}>Load Data</button>
+    </div>
+  )
+}
+
+export default App
